21b54d2db3678df4a6373a5e82be71843a301e73
revanorion <revanorion@gmail.com>
2017-10-04 09:20:08 -0400

    Updated index

--- CodeTablesDemo/ClientApp/store/index.ts
+++ CodeTablesDemo/ClientApp/store/index.ts
@@ -9,8 +9,6 @@ export interface ApplicationState {
     externalPaymentTypes: ExternalPaymentTypes.ExternalPaymentTypesState;  
 }
 
-
-
 // Whenever an action is dispatched, Redux will update each top-level application state property using
 // the reducer with the matching name. It's important that the names match exactly, and that the reducer
 // acts on the corresponding ApplicationState property type.
